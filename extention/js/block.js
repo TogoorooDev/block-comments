4d68af6dfef92a9fd6c9820c75ab8e63a33cd239
Mickenpox <robhensley10@gmail.com>
2019-03-03 17:58:01 -0800

    Update block.js

--- extention/js/block.js
+++ extention/js/block.js
@@ -1,5 +1,2 @@
-var comment_section = document.body.getElementsByTagName("ytd-comments"), index;
-
-for (index = comment_section.length - 1; index >= 0; index--) {
-    comment_section[index].parentNode.removeChild(comment_section[index]);
-}
+var comments = document.body.getElementById("comments");
+comments.innerHTML = "";
